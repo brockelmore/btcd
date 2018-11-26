128c52df7cf33cef5c9b75001e040687c1d02001
brockelmore <31553173+brockelmore@users.noreply.github.com>
2018-11-25 22:38:01 -0500

    Update helpers_test.go

--- btcjson/helpers_test.go
+++ btcjson/helpers_test.go
@@ -8,7 +8,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcjson"
+	"github.com/brockelmore/btcd/btcjson"
 )
 
 // TestHelpers tests the various helper functions which create pointers to
